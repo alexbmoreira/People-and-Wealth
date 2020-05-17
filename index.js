426c06b9cac97466c6efffad497efc526f4b9236
Alex Moreira <alexbmoreira@gmail.com>
2020-05-17 19:36:56 -0400

    Set up script Declared necessary DOM elements

---
+++ index.js
@@ -0,0 +1,9 @@
+const main = document.getElementById("main");
+const add_btn = document.getElementById("add");
+const double_btn = document.getElementById("double");
+const millionaires_btn = document.getElementById("millionaires");
+const sort_btn = document.getElementById("sort");
+const calc_btn = document.getElementById("calculate");
+
+let people = [];
+
