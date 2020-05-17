ac8d6aafb2cda333a4866737beb9e1702c902009
Alex Moreira <alexbmoreira@gmail.com>
2020-05-17 19:52:34 -0400

    Three random people added on load

--- index.js
+++ index.js
@@ -7,3 +7,47 @@ const calc_btn = document.getElementById("calculate");
 
 let people = [];
 
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Get random user and give them money
+async function getRandomUser()
+{
+    const res = await fetch("https://randomuser.me/api");
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const new_user = {
+        name: `${user.name.first} ${user.name.last}`,
+        wealth: (Math.random() * 1000000).toFixed(2)
+
+    }
+
+    addPerson(new_user);
+}
+
+// Add to peaople array
+function addPerson(object)
+{
+    people.push(object);
+
+    updateDOM();
+}
+
+// Update DOM
+function updateDOM(provided = people)
+{
+    // Clear main
+    main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+    provided.forEach(person =>
+    {
+        const person_div = document.createElement("div");
+        person_div.classList.add("person");
+        person_div.innerHTML = `<strong>${person.name}</strong> ${person.wealth}`;
+
+        main.appendChild(person_div);
+    });
+}
